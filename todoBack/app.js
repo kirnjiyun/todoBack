9cd15cd769ad9cab77988fe6b29683993e7e6f94
kirnjiyun <kimjiyunee@naver.com>
2024-05-05 22:15:54 +0900

    라우터

--- todoBack/app.js
+++ todoBack/app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-
+const indexRouter = require("./routes/index");
 const app = express();
 app.use(bodyParser.json());
-
+app.use("/api", indexRouter);
 const mongoURI = `mongodb://localhost:27017/todo-demo`;
 mongoose
     .connect(mongoURI, { useNewUrlParser: true })
@@ -12,6 +12,4 @@ mongoose
     .catch((err) => {
         console.log("DB connection fail", err);
     });
-app.listen(123, () => {
-    console.log("server is on 123");
-});
+app.listen(3000, () => console.log("Server is running on port 3000"));
