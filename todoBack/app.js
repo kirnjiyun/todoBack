e84678ad04230aae6c853eaf21a6683d87f3490f
kirnjiyun <kimjiyunee@naver.com>
2024-05-07 00:23:46 +0900

    Backend

--- todoBack/app.js
+++ todoBack/app.js
@@ -1,15 +1,25 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const indexRouter = require("./routes/index");
 const app = express();
+
+// CORS 미들웨어를 먼저 적용
+app.use(cors());
+
+// 그 다음 바디 파서
 app.use(bodyParser.json());
+
+// 그리고 API 라우터
 app.use("/api", indexRouter);
+
 const mongoURI = `mongodb://localhost:27017/todo-demo`;
 mongoose
     .connect(mongoURI, { useNewUrlParser: true })
-    .then(() => {})
+    .then(() => console.log("Database connected successfully"))
     .catch((err) => {
         console.log("DB connection fail", err);
     });
+
 app.listen(3000, () => console.log("Server is running on port 3000"));
