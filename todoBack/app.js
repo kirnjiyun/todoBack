d2e2ced43d8fb12f37471843e9bdb217995fda66
kirnjiyun <kimjiyunee@naver.com>
2024-05-10 01:25:20 +0900

    aws 오류

--- todoBack/app.js
+++ todoBack/app.js
@@ -25,4 +25,6 @@ mongoose
         console.log("DB connection fail", err);
     });
 
-app.listen(3000, () => console.log("Server is running on port 3000"));
+app.listen(process.env.PORT || 5000, () =>
+    console.log("Server is running on port 5000")
+);
