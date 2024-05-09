5c571ffc61e0a57fee3dafa1668e2f6f5e3a770d
kirnjiyun <kimjiyunee@naver.com>
2024-05-10 03:04:32 +0900

    그냥 프록시 넣기

--- todoFront/src/utils/api.js
+++ todoFront/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: `${process.env.REACT_APP_BACKEND_PROXY}/api`,
+    baseURL: "https://jiyun-todo.netlify.app/api",
     headers: {
         "Content-Type": "application/json",
     },
