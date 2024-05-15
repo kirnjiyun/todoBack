192733fa77e88c712aea757d6b01a91b19d1cebd
kirnjiyun <kimjiyunee@naver.com>
2024-05-15 23:43:54 +0900

    작성자

--- todoBack/controller/task.controller.js
+++ todoBack/controller/task.controller.js
@@ -4,7 +4,8 @@ const Task = require("../model/Task");
 taskController.createTask = async (req, res) => {
     try {
         const { task, isComplete } = req.body;
-        const newTask = new Task({ task, isComplete });
+        const { userId } = req;
+        const newTask = new Task({ task, isComplete, author: userId });
         await newTask.save();
         res.status(200).json({ status: "ok", data: newTask });
     } catch (err) {
@@ -18,7 +19,7 @@ taskController.createTask = async (req, res) => {
 
 taskController.getTask = async (req, res) => {
     try {
-        const taskList = await Task.find({}).select("-__v");
+        const taskList = await Task.find({}).select("-__v").populate("author");
         res.status(200).json({ status: "ok", data: taskList });
     } catch (err) {
         console.error(err); // 서버 로그에 에러를 출력
