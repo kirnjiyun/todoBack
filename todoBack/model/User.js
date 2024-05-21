367b610b9890b3801320620e2f6e6b21dab676d0
kirnjiyun <kimjiyunee@naver.com>
2024-05-22 02:43:45 +0900

    user

--- todoBack/model/User.js
+++ todoBack/model/User.js
@@ -1,29 +1,26 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+
+const Schema = mongoose.Schema;
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || "kimjiyunee";
 
-const userSchema = Schema(
+const userSchema = new Schema(
     {
         name: { type: String, required: true },
-        email: {
-            type: String,
-            required: true,
-        },
-        password: {
-            type: String,
-            required: true,
-        },
+        email: { type: String, required: true },
+        password: { type: String, required: true },
     },
     { timestamps: true }
 );
+
 userSchema.methods.toJSON = function () {
-    const obj = this._doc;
+    const obj = this.toObject();
     delete obj.updatedAt;
     delete obj.__v;
     return obj;
 };
+
 userSchema.methods.generateToken = function () {
     const token = jwt.sign({ _id: this._id }, JWT_SECRET_KEY, {
         expiresIn: "1d",
